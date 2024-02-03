b4d9da77364b59bd51e243502bd28afeda1995de
Amalia <dann.amalia04@gmail.com>
2024-02-03 15:42:00 +0100

    Deleted dice

--- dice.py
+++
@@ -1,8 +0,0 @@
-import random
-class Dice:
-    def __init__(self):
-        self.number = self.roll_dice()
-    def roll_dice(self):
-        rand = random.randint(1,6)
-        self.number = rand
-        return rand
