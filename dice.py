352705f0d27d695495df8f347679c0296eff621e
Amalia Dann <125802798+Amalia-Dann@users.noreply.github.com>
2024-02-03 15:24:05 +0100

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,8 @@
+import random
+class Dice:
+    def __init__(self):
+        self.number = self.roll_dice()
+    def roll_dice(self):
+        rand = random.randint(1,6)
+        self.number = rand
+        return rand
